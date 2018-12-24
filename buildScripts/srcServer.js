2f7371097f2fbbb695c3b00c427b5be7db6853b8
yamenfyi <yamenfyi@gmail.com>
2018-12-24 11:40:16 -0500

    module-7

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,21 @@
+// var express = require('express');
+// var path = require('path');
+// var open = require('open');
+import express from 'express'; // import introduced in ES6
+import path from 'path';
+import open from 'open';
+
+const port = 3000; // const introduced in ES6
+const app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname ,'../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open ('http://localhost:' + port);
+  }
+});
